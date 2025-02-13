104dbb50fee5ec2f6e579d6d504fd27c14fc8b14
Youssef <yousseftahertemraz.com>
2025-02-14 01:22:54 +0200

    finalize homepage styling

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains:['images.pexels.com', 'example.com']
-    }
+    // images: {
+    //     domains:['images.pexels.com', 'example.com']
+    // }
 };
 
 export default nextConfig;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,16 +4,16 @@ import Hero from '../../public/hero.png'
 
 export default function Home() {
   return (
-    <div className={styles.container}> 
+    <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Better Design for your Digital Products</h1>
-        <p className={styles.description}>  Craft intuitive and visually stunning digital experiences that captivate your audience. Our design solutions blend creativity with functionality, 
-          ensuring your digital products stand out while delivering seamless user interactions</p>
-          <button className={styles.button}>See our works</button>
+        <h1 className={styles.title}>Better Design for you</h1>
+        <p className={styles.desc}>  Craft intuitive and visually stunning digital experiences that captivate your audience. Our design solutions blend creativity with functionality,
+        </p>
+        <button className={styles.button}>See our works</button>
       </div>
-      <div className={styles.item}></div>
-
-      <Image src={Hero} alt='hhh' className={styles.img}  />
+      <div className={styles.item}>
+        <Image src={Hero} alt='hhh' className={styles.img} />
+      </div>
     </div>
   )
 
