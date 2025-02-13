df42a8f60323991a2c4ef4b54ef9a431ae5791ee
Youssef <yousseftahertemraz.com>
2025-02-14 00:36:56 +0200

    styling home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,15 @@ import Hero from '../../public/hero.png'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}> 
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better Design for your Digital Products</h1>
+        <p className={styles.description}>  Craft intuitive and visually stunning digital experiences that captivate your audience. Our design solutions blend creativity with functionality, 
+          ensuring your digital products stand out while delivering seamless user interactions</p>
+          <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}></div>
+
       <Image src={Hero} alt='hhh' className={styles.img}  />
     </div>
   )
