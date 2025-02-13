50462d5210156b730758e0768dbf9b9aa212a40e
Youssef <yousseftahertemraz.com>
2025-02-13 06:58:49 +0200

    adding footer social media images

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 const Footer = () => {
     return (
-        <div>
-            <div className={styles.container}>@2025 Youssef 1st next js project</div>
-      
+
+        <div className={styles.container}>
+
+            <div>@2025 Youssef 1st next js project</div>
+            <div className={styles.social}>
+                <Image src="/1.png" alt='youssef1' width={15} height={15} className={styles.icon}/>
+                <Image src="/2.png" alt='youssef1' width={15} height={15} className={styles.icon}/>
+                <Image src="/3.png" alt='youssef1' width={15} height={15} className={styles.icon}/>
+                <Image src="/4.png" alt='youssef1' width={15} height={15} className={styles.icon}/>
+            </div>
         </div>
     )
 }
