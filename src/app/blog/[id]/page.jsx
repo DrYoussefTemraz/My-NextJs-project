cd1db453d7d998bf2a8225bf50223b05575df767
Youssef <yousseftahertemraz.com>
2025-02-16 01:59:12 +0200

    fetching data in server side using fetch and async await

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import styles from "./page.module.css"
 import Image from "next/image";
-const BlogPost = () => {
+import { notFound } from "next/navigation";
+
+
+async function getData(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        cache: "no-store",
+    });
+    // for previous fetching we use {cache: "no-store"} as data is changing all the time 
+    // if we need to revalidate the data we use {next:{revalidate:10}}
+
+    if (!res.ok) {
+        return notFound()
+    }
+
+    return res.json();
+
+}
+const BlogPost = async ({ params }) => {
+    const { id } = params;
+    const data = await getData(id);
     return (
         <div className={styles.container}>
             <div className={styles.top}>
                 <div className={styles.info}>
-                    <h1 className={styles.title}>Lorem, ipsum dolor sit amet </h1>
+                    <h1 className={styles.title}>{data.title} </h1>
                     <p className={styles.desc}>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ullam! Magni, recusandae voluptates aperiam illum repellendus fuga provident mollitia repudiandae temporibus qui laboriosam voluptas, ducimus, tempore rem ipsum eos consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error perferendis enim voluptatibus quas ad, ullam officia asperiores quis quo. Reiciendis assumenda expedita aspernatur minus cumque adipisci! Temporibus optio illum quis!
                     </p>
