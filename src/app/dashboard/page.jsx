fea3cd9b7d2dc09873dc17db7405e4f462562365
Youssef <yousseftahertemraz.com>
2025-02-19 00:17:04 +0200

    fetchin data using swr for the client side

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,31 +2,42 @@
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css"
 import Error from "next/error";
+import useSWR from "swr";
 const Dashboard = () => {
-    const [data, setData] = useState([])
-    const [error, setError] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
-    //OLD WAY TO FETCH DATA
-    useEffect(() => {
-        const getData = async () => {
-            setIsLoading(true)
-            const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
-                cache: "no-store",
-            });
-            // for previous fetching we use {cache: "no-store"} as data is changing all the time 
-            // if we need to revalidate the data we use {next:{revalidate:10}}
+    
+        //OLD WAY TO FETCH DATA
+    // const [data, setData] = useState([])
+    // const [error, setError] = useState(false)
+    // const [isLoading, setIsLoading] = useState(false)
 
-            if (!res.ok) {
-                setError(true)
-            }
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         setIsLoading(true)
+    //         const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+    //             cache: "no-store",
+    //         });
+    //         // for previous fetching we use {cache: "no-store"} as data is changing all the time 
+    //         // if we need to revalidate the data we use {next:{revalidate:10}}
 
-            const data = await (res.json())
+    //         if (!res.ok) {
+    //             setError(true)
+    //         }
 
-            setData(data)
-            setIsLoading(false)
-        }
-        getData()
-    }, [])
+    //         const data = await (res.json())
+
+    //         setData(data)
+    //         setIsLoading(false)
+    //     }
+    //     getData()
+    // }, [])
+
+    //** using SWR for fetching data [SWR is a popular React library for data fetching].
+
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+    const { data, error, isLoading } = useSWR (`https://jsonplaceholder.typicode.com/posts`, fetcher)
+
+
+
 
     console.log(data)
 
