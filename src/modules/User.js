ae102346e84732d43efca401f0ac2172ee1ebca8
Youssef <yousseftahertemraz.com>
2025-02-20 00:54:03 +0200

    link the application with mongodb database

---
+++ src/modules/User.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const { Schema } = mongoose
+const userSchema = new Schema(
+    {
+    name: {
+        type: string,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: string,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: string,
+        required: true,
+    },
+
+}, { timestamps: true }
+)
+export default mongoose.model("user", userSchema );
--- src/utils/db.js
+++ src/utils/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose"
 
 const connect = async () => {
     try {
+         // Suppress the strictQuery deprecation warning
+         mongoose.set("strictQuery", false);
+
+         
         await mongoose.connect(process.env.MONGO);
     } catch (error) {
         // handleError(error);
