8b52bce186b9e23cc17947c1dfd563da3a35164c
Youssef <yousseftahertemraz.com>
2025-02-22 22:52:12 +0200

    launch register and sync database and format responses

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,8 +4,6 @@ const connect = async () => {
     try {
          // Suppress the strictQuery deprecation warning
          mongoose.set("strictQuery", false);
-
-         
         await mongoose.connect(process.env.MONGO);
     } catch (error) {
         // handleError(error);
