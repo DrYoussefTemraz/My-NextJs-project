5317664cfb61a391a7b71031fa9a5fd828ca111c
Youssef <yousseftahertemraz.com>
2025-02-22 17:32:45 +0200

    form of registration and login implemetation

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,41 @@
+"use client"
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
+import Button from '@/components/Botton/Button'
 
 const Register = () => {
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        const name = e.target[0].value
+
+    }
     return (
-        <div className={styles.container}>Register</div>
+        <div className={styles.container}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='username '
+                    className={styles.input}
+                    required />
+                <input
+                    type='email'
+                    placeholder='email '
+                    className={styles.input}
+                    required />
+                <input
+                    type='password'
+                    placeholder='password '
+                    className={styles.input}
+                    required />
+                <button
+                    className={styles.button}>
+                    Register
+                </button>
+         
+            </form>
+            <Link href="/dashboard/login">log in with existing account</Link>
+        </div>
     )
 }
 
